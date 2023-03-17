3dbbac75345b73e745a46e03986b9a0ebf19c549
Grekzxc <cool.grek456852@yandex.by>
2023-03-17 10:27:24 +0300

    useState

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,38 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Accordion from './components/accordion/Accordion';
+import Rating from './components/Rating/Rating';
+import { title } from 'process';
+import { OnOff1 } from './components/OnOff/OnOff';
 
 function App() {
+  console.log('App rendering');
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {/* <PageTitle title ={'This is APP component '} />
+      <PageTitle title ={'My friend '} /> */}
+
+      <Rating value={3} />
+      <Accordion titleValue={'Menu'} collapsed={true} />
+      <Accordion titleValue={'Users'} collapsed={false} />
+      < OnOff1 on={true} />
+      < OnOff1 on={false} />
+      <Rating value={0} />
+      <Rating value={1} />
+      <Rating value={2} />
+      <Rating value={3} />
+      <Rating value={4} />
+      <Rating value={5} />
     </div>
   );
+}
+
+type PageTitlePropsType = {
+  title: string
+}
+
+function PageTitle(props: PageTitlePropsType) {
+  return <div>{props.title}</div >
 }
 
 export default App;
---
+++ src/components/OnOff/OnOff.tsx
@@ -0,0 +1,18 @@
+import '../../App.css';
+
+type PtopsType = {
+    on: boolean
+}
+
+export function OnOff1(props: PtopsType) {
+    return (
+        <div></div>
+    )
+}
+
+
+export function OnOff2(props: PtopsType) {
+    return (
+        <button></button>
+    )
+}
